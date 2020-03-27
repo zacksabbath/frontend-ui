15326d17a756038e6d028117ea2126a01d3f49fc
Zachary <zachary.f.rose@gmail.com>
2020-03-27 00:47:47 -0600

    Implement Avatar component

---
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AvatarWrapper = styled.div`
+    img {
+        vertical-align: middle;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+    }
+`;
+
+type AvatarProps = {
+    image: string,
+};
+
+export default function Avatar(props:AvatarProps){
+    const {image} = props;
+    return (
+        <AvatarWrapper>
+            <img src={image} />
+        </AvatarWrapper>
+    );
+}
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,1 +1,2 @@
 export {default as Bubble} from './Bubble/Bubble';
+export {default as Avatar} from './Avatar/Avatar';
