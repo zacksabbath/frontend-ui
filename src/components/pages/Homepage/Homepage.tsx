0c7cc8eb11e9f0be21499af22668254ab3d49cbf
Tiana <Tiana.marie013@gmail.com>
2020-04-02 13:00:50 -0600

    card div titles

--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -4,7 +4,15 @@ export default function Homepage(props:any) {
     
     return (
         <div className="homepage-main">
-            Hello World
+            <div className="homepage-A1">
+                <h2>My Voting Ballot</h2>
+            </div>
+            <div className="homepage-A2">
+                <h2>Browse Issues</h2>
+            </div>
+            <div className="homepage-B">
+                <h2> News & Activities</h2>
+            </div>
         </div>
     )
 }
