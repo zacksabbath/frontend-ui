dd9ae6843a932f3f5ecba3f44ea8e88b3ce8a713
Tiana <Tiana.marie013@gmail.com>
2020-04-03 13:59:13 -0600

    css :

--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -4,14 +4,19 @@ import {ImageButton} from 'components';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ContainerWrapper = styled.div`
+    
+
     .logo img { 
-        height: 100%;
-        width: 100%;
+        width: 50%;
+        margin: 80px;
         
         }
-    // .container {
-    //     display: flex;
-    // }
+    .container {    
+        display: flex;
+        margin: 100px;
+        justify-content: space-between;
+     }
+     
 
 `;
 
@@ -23,13 +28,12 @@ export default function Homepage(props:any) {
                 <div className="logo">
             <img src="http://stump.zackrose.net/images/stump_logo.png" />
                 </div>
-                <div className="container">
-                    <ImageButton text="My Voting Ballot" image="http://stump.zackrose.net/images/voting_ballot.png"/>
+                <div className="container" >
+                    <ImageButton text="My Voting Ballot" image="http://stump.zackrose.net/images/voting_ballot.png"/>                  
                     <ImageButton text="Browse Issues" image="http://stump.zackrose.net/images/issues.png"/> 
                 </div>
             </ContainerWrapper>
             <ImageButton text="News & Activities" image="http://stump.zackrose.net/images/news.png"/>
-
         </div>
     )
 }
