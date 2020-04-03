ee614bb2f86a9a4b8d448796d3a1337ed35a62ca
Tiana <Tiana.marie013@gmail.com>
2020-04-03 17:56:29 -0600

    advice changes

--- src/components/atoms/ImageButton/ImageButton.tsx
+++ src/components/atoms/ImageButton/ImageButton.tsx
@@ -1,41 +1,35 @@
-import styled from 'styled-components';
-import React from 'react';
+import styled from "styled-components";
+import React from "react";
 
-const ImageButtonWrapper = styled.div`
-img{
+const ImageButtonWrapper = styled.button`
+  position: relative;
+  text-align: center;
+  image {
     height: 100%;
     width: 100%;
     object-fit: contain;
-    text {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-    }
-    :hover {
-        background-color: orange;
-    }
-    
+    position: absolute;
+    top: 0;
+  }
+  :hover {
+    background-color: orange;
+  }
+  text {
+    position: absolute;
+  }
 `;
 
-type ImageButtonProps  = {
-    image: string,
-    text: string,
+type ImageButtonProps = {
+  image: string;
+  text: string;
 };
 
-
-
-export default function ImageButton(props:ImageButtonProps) {
-    const { image, text } = props
-    return (
+export default function ImageButton(props: ImageButtonProps) {
+  const { image, text } = props;
+  return (
     <ImageButtonWrapper>
-        <img src={image}/>
-            <p className="text">
-                 {text}
-            </p>
+      <img src={image} />
+      <p className="text">{text}</p>
     </ImageButtonWrapper>
-    )
-   
-};
-
+  );
+}
--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import styled from 'styled-components';
-import { ImageButton } from 'components';
+import React from "react";
+import styled from "styled-components";
+import { ImageButton } from "components";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ContainerWrapper = styled.div`
-    .logo img { 
-        width: 50%;
-        margin: 80px;        
-    }
+  .logo img {
+    width: 50%;
+    margin: 80px;
+  }
+  .container {
+    display: flex;
+  }
 `;
 
-export default function Homepage(props:any) {
-    
-    return (
-        <div>
-            <ContainerWrapper>
-                <div className="logo">
-            <img src="http://stump.zackrose.net/images/stump_logo.png" />
-                </div>
-                <div className="top" >
-                    <ImageButton text="My Voting Ballot" image="http://stump.zackrose.net/images/voting_ballot.png" />                  
-                    <ImageButton text="Browse Issues" image="http://stump.zackrose.net/images/issues.png"/> 
-                </div>
-            </ContainerWrapper>
-            <ImageButton text="News & Activities" image="http://stump.zackrose.net/images/news.png"/>
-        </div>
-    )
-};
+export default function Homepage(props: any) {
+  return (
+    <ContainerWrapper>
+      <div className="logo">
+        <img src="http://stump.zackrose.net/images/stump_logo.png" />
+      </div>
+      <div className="container">
+        <ImageButton
+          text="My Voting Ballot"
+          image="http://stump.zackrose.net/images/voting_ballot.png"
+        />
+        <ImageButton
+          text="Browse Issues"
+          image="http://stump.zackrose.net/images/issues.png"
+        />
+      </div>
+      <ImageButton
+        text="News & Activities"
+        image="http://stump.zackrose.net/images/news.png"
+      />
+    </ContainerWrapper>
+  );
+}
