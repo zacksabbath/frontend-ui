971b7bb8257effa17e5995b860e86a63290583c1
Zachary <zachary.f.rose@gmail.com>
2020-04-08 21:50:21 -0600

    Add user icon to library

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,11 @@ import {
   faThumbsDown,
   faEllipsisH,
   faCalendarAlt,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
 // import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
 
-library.add(faThumbsUp, faThumbsDown, faEllipsisH, faCalendarAlt);
+library.add(faThumbsUp, faThumbsDown, faEllipsisH, faCalendarAlt, faUser);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
