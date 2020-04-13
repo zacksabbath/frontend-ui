7490c138e7a0adfeda3cc83786f44d38bd816d0e
Zachary <zachary.f.rose@gmail.com>
2020-04-13 13:04:05 -0600

    font name

--- src/components/molecules/NewsfeedItem/NewsfeedItem.tsx
+++ src/components/molecules/NewsfeedItem/NewsfeedItem.tsx
@@ -20,7 +20,8 @@ export type NewsfeedItemProps = {
 
 const CardWrapper = styled.div`
   width: 100%;
-  font-family: GalanoGrotesque, Futura, 'Trebuchet MS', Arial, sans-serif;
+  font-family: 'Galano Grotesque Regular', Futura, 'Trebuchet MS', Arial,
+    sans-serif;
   background-color: ${({ theme }) => theme.cardTheme?.primary};
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.3);
   border-radius: 8px;
