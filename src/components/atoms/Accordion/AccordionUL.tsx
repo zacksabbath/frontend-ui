8917f52012089c7cdb3cdd2816cd40ad9e71a16f
Pierce Morrill <piercemorrill91@gmail.com>
2020-04-14 10:22:49 -0600

    Have buttons listed with proper names

--- src/components/atoms/Accordion/AccordionUL.tsx
+++ src/components/atoms/Accordion/AccordionUL.tsx
@@ -21,43 +21,9 @@ const Main = styled.button`
   }
 `;
 
-//this is inplace of the API
-const issues = [
-  {
-    name: 'Economy',
-    subIssues: [],
-  },
-  {
-    name: 'Education',
-    subIssues: [],
-  },
-  {
-    name: 'Environment',
-    subIssues: [
-      {
-        name: 'Air Quality',
-      },
-      {
-        name: 'Flooding',
-      },
-      {
-        name: 'Fracking',
-      },
-    ],
-  },
-  {
-    name: 'Government',
-    subIssues: [],
-  },
-  {
-    name: 'Health',
-    subIssues: [],
-  },
-  {
-    name: 'Social',
-    subIssues: [],
-  },
-];
+interface IssuesProps {
+  issues?: IssuesProps[];
+}
 
 export default function AccordionUL({
   issues,
