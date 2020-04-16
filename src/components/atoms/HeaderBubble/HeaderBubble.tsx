659c33527736110dd9fa4ca4b8c07b39eb8806f5
Tiana <Tiana.marie013@gmail.com>
2020-04-16 11:04:57 -0600

    header

---
+++ src/components/atoms/HeaderBubble/HeaderBubble.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HeaderBubble = styled.button`
+  border-radius: 50px;
+`;
+
+type HeaderBubbleProps = {
+  onClick: any;
+};
+
+export default HeaderBubble;
