e4c03d191de9527b99819966c56d4779cd13ac31
Tiana <Tiana.marie013@gmail.com>
2020-04-16 11:18:59 -0600

    mdx

--- src/components/atoms/HeaderBubble/HeaderBubble.tsx
+++ src/components/atoms/HeaderBubble/HeaderBubble.tsx
@@ -1,11 +1,28 @@
 import styled from 'styled-components';
+import React from 'react';
 
-const HeaderBubble = styled.button`
+const HeaderBubbleWrapper = styled.button`
   border-radius: 50px;
 `;
 
 type HeaderBubbleProps = {
   onClick: any;
+  text: string;
 };
 
-export default HeaderBubble;
+const ImageText = styled.div`
+  color: white;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default function HeaderBubble(props: HeaderBubbleProps) {
+  const { onClick, text } = props;
+  return (
+    <HeaderBubbleWrapper onClick={onClick}>
+      <ImageText>{text}</ImageText>
+    </HeaderBubbleWrapper>
+  );
+}
