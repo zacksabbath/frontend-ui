9ea0390e5bbce2d6fd63c5c1c3a581bbf30efb9f
Tiana <Tiana.marie013@gmail.com>
2020-04-18 10:45:46 -0600

    header rebuild

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const HeaderWrapper = styled.div``;
+
 export default function Header() {
-  return <div></div>;
+  return <HeaderWrapper></HeaderWrapper>;
 }
