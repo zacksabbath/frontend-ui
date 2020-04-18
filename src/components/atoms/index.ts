f507bd3128cc3de7cd8031143e7066ae902a26f6
Tiana <Tiana.marie013@gmail.com>
2020-04-18 14:49:22 -0600

    Header re build added syled div

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,4 +2,3 @@ export { default as Avatar } from './Avatar/Avatar';
 export { default as Bubble } from './Bubble/Bubble';
 export { default as Clickable } from './Clickable/Clickable';
 export { default as ImageButton } from './ImageButton/ImageButton';
-export { default as HeaderBubble } from './HeaderBubble/HeaderBubble';
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Avatar } from 'components';
 
-const HeaderWrapper = styled.div``;
+const HeaderWrapper = styled.div`
+  position: absolute;
+  align-items: center;
+  width: 100%;
+  background: ${({ theme }) => theme.main.colors.purpleDark};
+`;
+
+const AvatarContainer = styled.div`
+  margin: 40px;
+  margin-right: 0px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const HeaderText = styled.p`
+  color: white;
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function Header() {
-  return <HeaderWrapper></HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <HeaderText>Local Issues</HeaderText>
+      <AvatarContainer>
+        <Avatar src="http://stump.zackrose.net/images/avatar_sm.png" />
+      </AvatarContainer>
+    </HeaderWrapper>
+  );
 }
