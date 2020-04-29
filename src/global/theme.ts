27cbb7c93cf3f186fd9794486c4e4ed66775face
Zachary <zachary.f.rose@gmail.com>
2020-04-28 23:03:07 -0600

    Update theme

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,41 +1,108 @@
-// Primary
+// Greyscale
+const white = '#FFFFFF';
+const black = '#000000';
+const lightGrey = '#EEEEEE';
+const middleGrey = '#AAAAAA';
+const darkGrey = '#333333';
+
+// Primary Colors
 const purple = '#6340B2';
 const purpleDark = '#3C266E';
 const purpleDarker = '#26154A';
 const purpleDarkest = '#1E103A';
-const pink = '#DA0064';
+const purpleLightText = '#A48ED4';
+const purpleDarkText = '#7764A1';
+
+const magenta = '#DA0064';
+
 const yellow = '#FFD52D';
 const yellowDark = '#DDC255';
-
-// Secondary
-const teal = '#77CCC5';
-const tealDark = '#5CA29D';
-const orange = '#FE8E4D';
-const orangeDark = '#DF7A3E';
-const aqua = '#4ECC8F';
-const aquaDark = '#3EA271';
-const violet = '#AB96C7';
-const violetDark = '#866CA9';
-const olive = '#9ACC64';
-const oliveDark = '#78A746';
-
-// Primary Text
-const purpleLightText = '#A48ED4';
-const purpleDarkText = '#604E86';
 const yellowDarkText = '#564810';
 
-// Secondary Text
-const tealDarkText = '#203735';
-const orangeDarkText = '#5E351C';
-const aquaDarkText = '#1E4F37';
-const violetDarkText = '#412C5E';
-const oliveDarkText = '#39521F';
+const green = '#46B918';
 
-// Greyscale
-const white = '#FFFFFF';
-const black = '#000000';
-const middleGrey = '#AAAAAA';
-const darkGrey = '#333333';
+// Secondary Colors
+const teal = '#77CCC5';
+const tealDark = '#5CA29D';
+const tealText = '#203735';
+
+const orange = '#FE8E4D';
+const orangeDark = '#DF7A3E';
+const orangeText = '#5E351C';
+const orangeLightText = '#AB5D30';
+
+const aqua = '#4ECC8F';
+const aquaDark = '#3EA271';
+const aquaText = '#1E4F37';
+
+const violet = '#AB96C7';
+const violetDark = '#866CA9';
+const violetText = '#412C5E';
+
+const olive = '#9ACC64';
+const oliveDark = '#78A746';
+const oliveText = '#39521F';
+
+export const palette = {
+  white,
+  black,
+  lightGrey,
+  middleGrey,
+  darkGrey,
+  purple,
+  purpleDark,
+  purpleDarker,
+  purpleDarkest,
+  purpleLightText,
+  purpleDarkText,
+  magenta,
+  yellow,
+  yellowDark,
+  yellowDarkText,
+  green,
+  teal,
+  tealDark,
+  tealText,
+  orange,
+  orangeDark,
+  orangeText,
+  orangeLightText,
+  aqua,
+  aquaDark,
+  aquaText,
+  violet,
+  violetDark,
+  violetText,
+  olive,
+  oliveDark,
+  oliveText,
+};
+
+export const spacing = {
+  sm: '10px',
+  med: '15px',
+  l: '20px',
+  xl: '25px',
+  xxl: '30px',
+  xxxl: '40px',
+};
+
+export const fontWeights = {
+  normal: 'normal',
+  medium: 500,
+  semiBold: 600,
+};
+
+const breakpoints = {
+  keys: ['xs', 'sm', 'md', 'lg', 'xl'],
+  values: {
+    xs: 0,
+    sm: 600,
+    md: 960,
+    lg: 1280,
+    xl: 1920,
+  },
+};
 
 // primary: Main card background
 // Secondary: ellipses and bubble background
@@ -43,45 +110,99 @@ const darkGrey = '#333333';
 // secondary text: Date in bottom right
 
 const news = {
-  primary: purpleDark,
-  secondary: purple,
-  text: white,
-  secondaryText: purpleLightText,
+  background: {
+    primary: orange,
+    secondary: orangeDark,
+  },
+  foreground: {
+    primary: orangeDark,
+    icon: white,
+  },
+  text: {
+    primary: orangeText,
+    secondary: white,
+    primaryLight: orangeLightText,
+  },
 };
 
+//question become quote
 const question = {
-  primary: teal,
-  secondary: tealDark,
-  text: black,
-  secondaryText: tealDarkText,
+  background: {
+    primary: teal,
+    secondary: tealDark,
+  },
+  foreground: {
+    primary: tealDark,
+    icon: white,
+  },
+  text: {
+    primary: tealText,
+    secondary: white,
+  },
 };
 
+//contribution becomes finance
 const contribution = {
-  primary: aqua,
-  secondary: aquaDark,
-  text: black,
-  secondaryText: tealDarkText,
+  background: {
+    primary: olive,
+    secondary: oliveDark,
+  },
+  foreground: {
+    primary: oliveDark,
+    icon: white,
+  },
+  text: {
+    primary: oliveText,
+    secondary: white,
+  },
 };
 
+//vote becomes legislation
 const vote = {
-  primary: white,
-  secondary: middleGrey,
-  text: black,
-  secondaryText: darkGrey,
+  background: {
+    primary: white,
+    secondary: lightGrey,
+  },
+  foreground: {
+    primary: lightGrey,
+    icon: middleGrey,
+  },
+
+  text: {
+    primary: black,
+    secondary: darkGrey,
+  },
 };
 
 const history = {
-  primary: orange,
-  secondary: orangeDark,
-  text: black,
-  secondaryText: orangeDarkText,
+  background: {
+    primary: aqua,
+    secondary: aquaDark,
+  },
+  foreground: {
+    primary: aquaDark,
+    icon: white,
+  },
+  text: {
+    primary: aquaText,
+    secondary: white,
+  },
 };
 
 const event = {
-  primary: violet,
-  secondary: violetDark,
-  text: white,
-  secondaryText: violetDarkText,
+  background: {
+    primary: violet,
+    secondary: violetDark,
+  },
+  foreground: {
+    primary: violetDark,
+    icon: white,
+  },
+  text: {
+    primary: violetText,
+    secondary: white,
+    primaryLight: white,
+  },
 };
 
 export const cardThemes = {
@@ -103,50 +224,51 @@ export type cardTypes =
 
 export const getCardTheme = (key: cardTypes) => cardThemes[key];
 
-export const colors = {
-  //greyscale
-  white,
-  black,
-  middleGrey,
-  darkGrey,
-
-  //primary
-  purple,
-  purpleDark,
-  purpleDarker,
-  purpleDarkest,
-  pink,
-  yellow,
-  yellowDark,
-
-  //secondary
-  teal,
-  tealDark,
-  orange,
-  orangeDark,
-  aqua,
-  aquaDark,
-  violet,
-  violetDark,
-  olive,
-  oliveDark,
-
-  //primary text
-  purpleLightText,
-  purpleDarkText,
-  yellowDarkText,
-
-  //secondary text
-  tealDarkText,
-  orangeDarkText,
-  aquaDarkText,
-  violetDarkText,
-  oliveDarkText,
+export const main = {
+  //purple theme
+  background: {
+    primary: purpleDarkest,
+    secondary: purpleDarker,
+    gradient: {
+      //primary gradient is brighter
+      primary: {
+        light: '#301D58',
+        dark: purpleDarker,
+        // lightBorder: '#3C266E',
+        // darkBorder: '#2D1A57'
+      },
+      //secondary gradient is more subtle
+      secondary: {
+        light: purpleDarker,
+        dark: '#221242',
+        // lightBorder: '#26154A',
+        // darkBorder: '#26154A'
+      },
+    },
+  },
+  foreground: {
+    //primary is darker
+    primary: purpleDark,
+    //secondary is brighter
+    secondary: purple,
+    icon: purple,
+  },
+  text: {
+    primary: white,
+    secondary: purpleLightText,
+    selected: white,
+    disabled: purpleDarkText,
+  },
+  action: {
+    selected: magenta,
+  },
 };
 
 export default {
-  main: {
-    colors,
-    cardThemes,
-  },
+  spacing,
+  palette,
+  fontWeights,
+  breakpoints,
+
+  ...main,
 };
