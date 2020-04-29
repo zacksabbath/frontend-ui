ad7cdf56a69f17e25fc0a9462960d7a365f81562
Zachary <zachary.f.rose@gmail.com>
2020-04-28 23:05:21 -0600

    Refactor Clickable with new theme

--- src/components/atoms/Clickable/Clickable.tsx
+++ src/components/atoms/Clickable/Clickable.tsx
@@ -1,8 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Clickable = styled.a`
-  font-weight: bold;
-  color: ${({ theme }) => theme.main.colors.pink};
-`;
+const Clickable = styled.a(({ theme }) => {
+  const { action, fontWeights } = theme;
+
+  return css`
+    font-weight: ${fontWeights.semiBold};
+    color: ${action.selected};
+  `;
+});
 
 export default Clickable;
