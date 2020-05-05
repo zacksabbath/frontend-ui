dc4b8965b8011efc766fc956f3c72664e1e23160
Wes Galbraith <galbwe92@gmail.com>
2020-05-04 20:55:25 -0600

    Add spatial layout of components

--- src/components/molecules/InitiativesCard/InitiativesCard.tsx
+++ src/components/molecules/InitiativesCard/InitiativesCard.tsx
@@ -1,6 +1,44 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 
+type InitiativesCardWrapperProps = {};
+
+const InitiativesCardWrapper = styled.div<InitiativesCardWrapperProps>(
+  props => {
+    const theme = props.theme;
+
+    return css`
+      background-color: ${theme.background.primary}
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: space-evenly;
+      .issues-card-top {
+        background: red;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+      }
+      .issues-card-mid {
+        background: white;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+      }
+      .issues-card-bottom {
+        background: blue;
+        color: white;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+      }
+    `;
+  }
+);
+
 type InitiativesCardProps = {
   initiativeName: string;
   issue: string;
@@ -11,12 +49,18 @@ type InitiativesCardProps = {
 
 export default function InitiativesCard(props: InitiativesCardProps) {
   return (
-    <div>
-      <div>Initiative Name: {props.initiativeName}</div>
-      <div>Issue: {props.issue}</div>
-      <div>Initiative Type: {props.initiativeType}</div>
-      <div>Votes For: {props.votesFor}</div>
-      <div>Votes Against: {props.votesAgainst}</div>
-    </div>
+    <InitiativesCardWrapper>
+      <div className={'issues-card-top'}>
+        <div>Initiative Type: {props.initiativeType}</div>
+        <div>Issue: {props.issue}</div>
+      </div>
+      <div className={'issues-card-mid'}>
+        <div>Initiative Name: {props.initiativeName}</div>
+      </div>
+      <div className={'issues-card-bottom'}>
+        <div>Votes For: {props.votesFor}</div>
+        <div>Votes Against: {props.votesAgainst}</div>
+      </div>
+    </InitiativesCardWrapper>
   );
 }
