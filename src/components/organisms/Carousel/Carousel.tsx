025f79a8edf52a91941d1b39b64a01815a94fad2
Zachary <zachary.f.rose@gmail.com>
2020-05-09 16:06:22 -0600

    Create Basic Carousel with left / right buttons

--- src/components/organisms/Carousel/Carousel.tsx
+++ src/components/organisms/Carousel/Carousel.tsx
@@ -1,63 +1,105 @@
-import React from 'react';
-import { useCarousel } from '../../../utils';
+import * as React from 'react';
+import styled, { css } from 'styled-components';
 
-export interface ICarousel {
-  slides: React.ReactNode[];
-  interval: number;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const SCarouselWrapper = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+`;
+
+interface ICarouselSlide {
+  active?: boolean;
 }
 
-export default function Carousel({ slides, interval = 5000 }: ICarousel) {
-  const length = slides.length;
-  const [active, setActive, handlers, style] = useCarousel(length, interval);
+const SCarouselSlide = styled.div<ICarouselSlide>`
+  flex: 0 0 auto;
+  opacity: ${props => (props.active ? 1 : 0)};
+  transition: all 0.5s ease;
+  width: 100%;
+`;
+
+interface ICarouselProps {
+  currentSlide: number;
+}
+
+const SCarouselSlides = styled.div<ICarouselProps>`
+  display: flex;
+  ${props =>
+    props.currentSlide &&
+    css`
+      transform: translateX(-${props.currentSlide * 100}%);
+    `};
+  transition: all 0.5s ease;
+`;
+
+interface IProps {
+  children: JSX.Element[];
+}
+
+const SlideButton = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 30px;
+  opacity: 0.5;
+  border: none;
+
+  :hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+`;
+
+const GoLeftButton = styled(SlideButton)`
+  left: 0;
+`;
+
+const GoRightButton = styled(SlideButton)`
+  right: 0;
+`;
+
+const Carousel = ({ children }: IProps) => {
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const activeSlide = children.map((slide, index) => (
+    <SCarouselSlide active={currentSlide === index} key={index}>
+      {slide}
+    </SCarouselSlide>
+  ));
 
   return (
-    length > 0 && (
-      <div className="carousel">
-        <ol className="carousel-indicators">
-          {slides.map((_, index) => (
-            <li
-              onClick={() => setActive(index)}
-              key={index}
-              className={`${active === index ? 'active' : ''}`}
-            />
-          ))}
-        </ol>
-        <div className="carousel-content" {...handlers} style={style}>
-          <div className="carousel-item">{slides[slides.length - 1]}</div>
-          {slides.map((slide, index) => (
-            <div className="carousel-item" key={index}>
-              {slide}
-            </div>
-          ))}
-          <div className="carousel-item">{slides[0]}</div>
-        </div>
-      </div>
-    )
+    <>
+      <SCarouselWrapper>
+        <SCarouselSlides currentSlide={currentSlide}>
+          {activeSlide}
+        </SCarouselSlides>
+      </SCarouselWrapper>
+      {currentSlide > 0 && (
+        <GoLeftButton
+          onClick={() => {
+            setCurrentSlide(
+              (currentSlide - 1 + activeSlide.length) % activeSlide.length
+            );
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </GoLeftButton>
+      )}
+      {currentSlide < children.length - 1 && (
+        <GoRightButton
+          onClick={() => {
+            setCurrentSlide((currentSlide + 1) % activeSlide.length);
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowRight} />
+        </GoRightButton>
+      )}
+    </>
   );
-}
+};
 
-// type People = {
-//   img: string;
-//   name: string;
-//   id: string;
-// };
-
-// interface CarouselProps {
-//   pageSize: number;
-//   people: People[];
-// }
-
-// const CarouselWrapper = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-// `;
-
-// export default function Carousel(props: CarouselProps) {
-//   const { pageSize, people } = props;
-//   const [page, setPage] = useState(0);
-
-//   const totalPages = Math.floor(people.length / pageSize);
-//   const content = people.slice(page * pageSize, pageSize);
-
-//   <CarouselWrapper>{}</CarouselWrapper>;
-// }
+export default Carousel;
