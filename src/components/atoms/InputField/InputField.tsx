911c5586b343095dcd7b871fee499dfbeee59240
Tiana <Tiana.marie013@gmail.com>
2020-05-13 19:03:42 -0600

    text added

--- src/components/atoms/InputField/InputField.tsx
+++ src/components/atoms/InputField/InputField.tsx
@@ -11,12 +11,12 @@ const InputWrapper = styled.div(({ theme }) => {
 
 const TextField = styled.input`
   padding: 6px 8px;
-  border-width: 1px;
-  border-style: solid;
+  // border-width: 1px;
+  // border-style: solid;
   border-radius: 10px;
   border-color: black;
   height: 20px;
-  margin: 0;
+  margin: 10px;
   :focus {
     border-width: 4px;
     border-color: magenta;
@@ -25,9 +25,21 @@ const TextField = styled.input`
   }
 `;
 
-export default function InputField() {
+const TextID = styled.p`
+  margin: 20px;
+  color: magenta;
+`;
+
+interface IInputIdentification {
+  text: string;
+}
+
+export default function InputField(props: IInputIdentification) {
+  const { text } = props;
+
   return (
     <InputWrapper>
+      <TextID>{text}</TextID>
       <TextField />
     </InputWrapper>
   );
