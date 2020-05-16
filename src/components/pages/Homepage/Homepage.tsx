c1677860ed5729afb87dcd9d277bd910a2947874
Zachary <zachary.f.rose@gmail.com>
2020-05-15 21:50:03 -0600

    Fix unused variable error

--- src/components/pages/Homepage/Homepage.tsx
+++ src/components/pages/Homepage/Homepage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { DefaultTemplate, Header, ImageButton } from '../../../components';
 import { useHistory } from 'react-router-dom';
 
