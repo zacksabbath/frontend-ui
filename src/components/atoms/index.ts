9676fc993c5501dbf449d09be441dc5699687e79
Zachary <zachary.f.rose@gmail.com>
2020-05-17 11:06:49 -0600

    export card

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,7 @@
 export * from './Avatar/Avatar';
 export { default as Avatar } from './Avatar/Avatar';
 export { default as Bubble } from './Bubble/Bubble';
+export { default as Card } from './Card/Card';
 export { default as Clickable } from './Clickable/Clickable';
 export { default as ImageButton } from './ImageButton/ImageButton';
 export { default as ScrollContainer } from './ScrollContainer/ScrollContainer';
