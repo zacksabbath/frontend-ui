2e0d677250acef7933d27fdff8fd10b3ef39d1cd
Zachary <zachary.f.rose@gmail.com>
2020-05-17 00:55:02 -0600

    back to basics

--- src/components/molecules/InitiativesCard/InitiativesCard.tsx
+++ src/components/molecules/InitiativesCard/InitiativesCard.tsx
@@ -1,43 +1,5 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-
-type InitiativesCardWrapperProps = {};
-
-const InitiativesCardWrapper = styled.div<InitiativesCardWrapperProps>(
-  props => {
-    const theme = props.theme;
-
-    return css`
-      background-color: ${theme.background.primary}
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: space-evenly;
-      .issues-card-top {
-        background: red;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-      }
-      .issues-card-mid {
-        background: white;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-      }
-      .issues-card-bottom {
-        background: blue;
-        color: white;
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: center;
-      }
-    `;
-  }
-);
+// import styled, { css } from 'styled-components';
 
 type InitiativesCardProps = {
   initiativeName: string;
@@ -47,31 +9,7 @@ type InitiativesCardProps = {
   votesAgainst: number;
 };
 
-// TODO: make this a class component and keep track of votesFor and
-// votesAgainst in state
 export default function InitiativesCard(props: InitiativesCardProps) {
-  return (
-    <InitiativesCardWrapper>
-      <div className={'issues-card-top'}>
-        <div className={'initiative-type'}>
-          Initiative Type: {props.initiativeType}
-        </div>
-        <div className={'issue'}>Issue: {props.issue}</div>
-      </div>
-      <div className={'issues-card-mid'}>
-        <div className={'initiative-name'}>
-          Initiative Name: {props.initiativeName}
-        </div>
-      </div>
-      <div className={'issues-card-bottom'}>
-        {/* TODO: make these clickable and add an onClick to increase the vote tally */}
-        <div className={'vote-tally votes-for'}>
-          Votes For: {props.votesFor}
-        </div>
-        <div className={'vote-tally votes-against'}>
-          Votes Against: {props.votesAgainst}
-        </div>
-      </div>
-    </InitiativesCardWrapper>
-  );
+  console.log(props);
+  return <div>InitiativesCard</div>;
 }
